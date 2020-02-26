65d7679376868088fb90c771625a91f59ba7def4
danivichkin <danivichkin@gmail.com>
2020-02-26 22:46:36 +0300

    Initial commit

--- src/main/java/lab7/Main.java
+++ src/main/java/lab7/Main.java
@@ -14,14 +14,25 @@ public class Main {
 
         HashMap<String, HashSet<String>> tables = new HashMap<>();
         Connection connection = getConnection();
+
         System.out.println("List of tables: ");
         List<String> tbls = getTables(connection);
         tbls.forEach(System.out::println);
 
+
+        for(String table: tbls){
+            System.out.println("List of table field " + table + ": ");
+            List<String> fields = getFields(connection, table);
+
+            HashSet<String> hashSet = new HashSet<>();
+            fields.forEach(f -> {
+                System.out.println(f);
+                hashSet.add(f);
+            });
+            tables.put(table, hashSet);
+        }
     }
-
-
-
+    
     public static Connection getConnection() throws SQLException, ClassNotFoundException {
         if (connection != null && !connection.isClosed()){
             return connection;
@@ -31,6 +42,27 @@ public class Main {
         connection = DriverManager.getConnection( dbURL, "postgres", "vfrae0v5");
 
         return connection;
+    }
+
+    public static List<String> getFields(Connection connection, String tableName) throws SQLException {
+        List<String> fields = new ArrayList<>();
+        PreparedStatement preparedStatement = connection.prepareStatement(
+                "SELECT a.attname " +
+                        "FROM pg_catalog.pg_attribute a " +
+                        "WHERE a.attrelid = (SELECT c.oid FROM pg_catalog.pg_class c " +
+                        "LEFT JOIN pg_catalog.pg_namespace n ON n.oid = c.relnamespace " +
+                        " WHERE pg_catalog.pg_table_is_visible(c.oid) AND c.relname = ? )" +
+                        " AND a.attnum > 0 AND NOT a.attisdropped");
+
+        preparedStatement.setString(1, tableName);
+        ResultSet resultSet = preparedStatement.executeQuery();
+
+        while (resultSet.next()){
+            String str = resultSet.getString("attname");
+            fields.add(str);
+        }
+        preparedStatement.close();
+        return fields;
     }
 
     public static List<String> getTables(Connection connection) throws SQLException {
